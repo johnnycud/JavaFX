ec77761143e55f3c4c5cb93ac88745a059f66355
jcuddles@msn.com <jcuddles@msn.com>
2018-04-11 14:26:13 +0100

    constructor and getters and setters

---
+++ src/com/teamtreehouse/pomodoro/model/Attempt.java
@@ -0,0 +1,29 @@
+package com.teamtreehouse.pomodoro.model;
+
+public class Attempt {
+    private String mMessage;
+    private int mRemainingSeconds;
+    private AttemptKind mKind;
+
+    public Attempt(String message, AttemptKind kind) {
+        mMessage = message;
+        mKind = kind;
+        mRemainingSeconds = kind.getTotalSeconds();
+    }
+
+    public String getMessage() {
+        return mMessage;
+    }
+
+    public int getRemainingSeconds() {
+        return mRemainingSeconds;
+    }
+
+    public AttemptKind getKind() {
+        return mKind;
+    }
+
+    public void setRemainingSeconds(int remainingSeconds) {
+        mRemainingSeconds = remainingSeconds;
+    }
+}
---
+++ src/com/teamtreehouse/pomodoro/model/AttemptKind.java
@@ -0,0 +1,18 @@
+package com.teamtreehouse.pomodoro.model;
+
+public enum AttemptKind {
+    FOCUS(25*60),
+    BREAK(5*60);
+
+    AttemptKind(int totalSeconds) {
+        mTotalSeconds = totalSeconds;
+    }
+
+    public int getTotalSeconds() {
+        return mTotalSeconds;
+    }
+
+    private int mTotalSeconds;
+
+    
+}
