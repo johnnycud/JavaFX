f2ef9d49e26edc8ec973b86a87d9803631461dcc
jcuddles@msn.com <jcuddles@msn.com>
2018-04-16 15:42:43 +0100

    binding controller and timer display

--- src/com/teamtreehouse/pomodoro/controllers/Home.java
+++ src/com/teamtreehouse/pomodoro/controllers/Home.java
@@ -2,6 +2,8 @@ package com.teamtreehouse.pomodoro.controllers;
 
 import com.teamtreehouse.pomodoro.model.Attempt;
 import com.teamtreehouse.pomodoro.model.AttemptKind;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -14,11 +16,38 @@ public class Home {
 
     private Attempt mCurrentAttempt;
 
+    private StringProperty mTimerText;
+
+    public Home() {
+        mTimerText = new SimpleStringProperty();
+        setTimerText(0);
+    }
+
+    public String getTimerText() {
+        return mTimerText.get();
+    }
+
+    public StringProperty timerTextProperty() {
+        return mTimerText;
+    }
+
+    public void setTimerText(String timerText) {
+        mTimerText.set(timerText);
+    }
+
+    public void setTimerText(int remainingSeconds){
+
+        int minutes = remainingSeconds/60;
+        int seconds = remainingSeconds %60;
+        setTimerText(String.format("%02d:%02d", minutes, seconds));
+    }
+
     private void prepareAttempt(AttemptKind kind){
         clearAttemptStyles();
         mCurrentAttempt = new Attempt(kind,"");
         addAttemptStyle(kind);
         title.setText(kind.getDisplayName());
+        setTimerText(mCurrentAttempt.getRemainingSeconds());
     }
 
     private void addAttemptStyle(AttemptKind kind) {
